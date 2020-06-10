541faf0074663ed5fdee7faab709e7e69e56b7de
Josh JR Reynaldo <joshuaronaldo088@gmail.com>
2020-06-10 14:07:33 +0700

    lecture 7

--- GoodNewsMVVMUdemy/Controller/NewsListTableViewController.swift
+++ GoodNewsMVVMUdemy/Controller/NewsListTableViewController.swift
@@ -11,4 +11,13 @@ import UIKit
 
 class NewsListTableViewController: UITableViewController{
 	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		setup()
+	}
+	
+	private func setup(){
+		self.navigationController?.navigationBar.prefersLargeTitles = true
+		
+	}
 }
