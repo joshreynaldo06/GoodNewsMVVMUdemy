09e5629d3b0bd7ce16ef5d9eae8e22c83e97a9ff
Josh JR Reynaldo <joshuaronaldo088@gmail.com>
2020-06-10 13:50:45 +0700

    initial commit

---
+++ GoodNewsMVVMUdemy/Controller/NewsListTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NewsListTableViewController.swift
+//  GoodNewsMVVMUdemy
+//
+//  Created by Josh JR Reynaldo on 10/06/20.
+//  Copyright © 2020 Josh JR Reynaldo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewsListTableViewController: UITableViewController{
+	
+}
