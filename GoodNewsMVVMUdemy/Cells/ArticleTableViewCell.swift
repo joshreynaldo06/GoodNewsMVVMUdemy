6a7f0ace76030323595d2af05e1357906349da67
Josh JR Reynaldo <joshuaronaldo088@gmail.com>
2020-06-10 15:28:30 +0700

    lecture 11

---
+++ GoodNewsMVVMUdemy/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNewsMVVMUdemy
+//
+//  Created by Josh JR Reynaldo on 10/06/20.
+//  Copyright © 2020 Josh JR Reynaldo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+	
+	@IBOutlet weak var titleLabel: UILabel!
+	@IBOutlet weak var descLabel: UILabel!
+}
